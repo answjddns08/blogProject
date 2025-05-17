efaf060609a378109ef3ced9d0bdbf97120c5a51
answjddns08 <answjddns0808@gmail.com>
2025-05-17 19:59:12 +0900

    간단한 로딩 css 제작

--- frontend/src/views/notFoundView.vue
+++ frontend/src/views/notFoundView.vue
@@ -1,8 +1,36 @@
 <template>
-  <main>
-    <p>hmm... it seems that there is a problem with the page.</p>
+  <main class="flex flex-col items-center">
+    <div class="flex items-center gap-x-1.5 text-7xl font-bold">
+      <span>4</span>
+      <span class="loading_circle"></span>
+      <span>4</span>
+    </div>
+    <p class="text-lg mt-4">Page Not Found</p>
   </main>
 </template>
 
 <script setup></script>
-<style scoped></style>
+<style scoped>
+.loading_circle {
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+
+  border: 0.6rem solid transparent;
+
+  border-top-color: #000;
+
+  animation: loading 1s ease infinite;
+
+  /* margin: 0 0.5rem; */
+}
+
+@keyframes loading {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
